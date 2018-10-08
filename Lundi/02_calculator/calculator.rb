5eb9c9cd8b509c02427a52eb438b64af5107cff8
victorhebdo <victor@gabella.org>
2018-10-08 17:23:09 +0200

    calculator

---
+++ Lundi/02_calculator/calculator.rb
@@ -0,0 +1,37 @@
+def add(x,y)
+
+  x + y
+
+end
+
+def subtract(x,y)
+
+  x - y
+
+end
+
+def sum(array)
+
+  array.sum
+
+end
+
+
+def multiply(x,y)
+
+  x*y
+
+end
+
+
+def power (x,y)
+  x**y
+end
+
+def factorial(x)
+
+
+    (1..x).inject(1, :*)
+
+
+end
